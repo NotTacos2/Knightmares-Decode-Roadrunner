4134980486254e39756cee8d54c82bb01dc16d1b
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2025-10-16 19:30:36 -0700

    flywheels pid test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/decode/motifs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/decode/motifs.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode.common.decode;
-
 public enum motifs {
     PPG,
     PGP,
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/Flywheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/Flywheels.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.common.subsystems;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.hardware.powerable.SetPower;
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.KineticState;
 import org.firstinspires.ftc.teamcode.common.Parts;
 
 public class Flywheels implements Subsystem {
@@ -11,10 +13,20 @@ public class Flywheels implements Subsystem {
 
     Parts part = new Parts();
 
+    ControlSystem flywheelsystem = ControlSystem.builder()
+            .posPid(0.02, 0, 0.02)
+            .build();
 
-    // idk names for these
     public Command powerone = new SetPower(Parts.flywheel1, 1).requires(this);
+    public Command testpowerone = new SetPower(Parts.flywheel1,
+            flywheelsystem.calculate(
+                    new KineticState(Parts.flywheel1.getCurrentPosition(), Parts.flywheel1.getVelocity())
+            )).requires(this);
     public Command powertwo = new SetPower(Parts.flywheel2, 1).requires(this);
+    public Command testpowertwo = new SetPower(Parts.flywheel2,
+            flywheelsystem.calculate(
+                    new KineticState(Parts.flywheel2.getCurrentPosition(), Parts.flywheel2.getVelocity())
+            )).requires(this);
     public Command negpowerone = new SetPower(Parts.flywheel1, -1).requires(this);
     public Command negpowertwo = new SetPower(Parts.flywheel2, -1).requires(this);
     public Command stopone = new SetPower(Parts.flywheel1, 0).requires(this);
