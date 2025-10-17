68f9f24f30f722aec18f397e9525e4d3c6f09499
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2025-10-16 19:39:08 -0700

    Update shooting.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commands/shooting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commands/shooting.java
@@ -9,19 +9,19 @@ import dev.nextftc.core.components.SubsystemComponent;
 import org.firstinspires.ftc.teamcode.common.subsystems.Pivot;
 import org.firstinspires.ftc.teamcode.common.subsystems.Flywheels;
 
-public class shooting {
-    public shooting() {
+public class shooting { // ok so tyler, this is basically shooting command for auto
+    public shooting() { // initalizes shooting by calling flywheels and pivot commands
         new SubsystemComponent(Pivot.INSTANCE, Flywheels.INSTANCE);
     }
 
     public Command autoRoutine() {
-        return new SequentialGroup(
-            Pivot.INSTANCE.reset,
-            new ParallelGroup(
-                Flywheels.INSTANCE.powerone,
-                Flywheels.INSTANCE.powertwo
+        return new SequentialGroup( // these are commands and sequentialgroup runs them one by one
+            Pivot.INSTANCE.reset, // calls the reset command so shooting works
+            new ParallelGroup( // parallelgroup runs them all
+                Flywheels.INSTANCE.powerone, // powers the flywheel which is one motor
+                Flywheels.INSTANCE.powertwo // powers the other flywheel
             ),
-            new Delay(0.5),
+            new Delay(0.5), // delays by 500 ms and we'll see how the balls go
             new ParallelGroup(
                  Flywheels.INSTANCE.stopone,
                  Flywheels.INSTANCE.stoptwo
@@ -49,3 +49,4 @@ public class shooting {
         );
     }
 }
+
