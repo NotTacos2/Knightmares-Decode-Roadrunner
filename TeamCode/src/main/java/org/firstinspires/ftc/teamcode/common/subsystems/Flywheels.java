1998e726f396c837c5aa00d849d6485d06fbc930
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2025-10-16 19:42:23 -0700

    Update Flywheels.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/Flywheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/Flywheels.java
@@ -1,3 +1,5 @@
+// This is our flywheels subsystem which is a group of commands
+
 package org.firstinspires.ftc.teamcode.common.subsystems;
 
 import dev.nextftc.core.commands.Command;
@@ -6,17 +8,18 @@ import dev.nextftc.hardware.powerable.SetPower;
 import org.firstinspires.ftc.teamcode.common.Parts;
 
 public class Flywheels implements Subsystem {
-    public static final Flywheels INSTANCE = new Flywheels();
+    public static final Flywheels INSTANCE = new Flywheels(); // makes this as group of commands
     private Flywheels() {}
 
     Parts part = new Parts();
 
 
     // idk names for these
-    public Command powerone = new SetPower(Parts.flywheel1, 1).requires(this);
+    public Command powerone = new SetPower(Parts.flywheel1, 1).requires(this); // powers this by 1 which is full speed and you can't go more otherwise it will be counted as one
     public Command powertwo = new SetPower(Parts.flywheel2, 1).requires(this);
-    public Command negpowerone = new SetPower(Parts.flywheel1, -1).requires(this);
+    public Command negpowerone = new SetPower(Parts.flywheel1, -1).requires(this); // powers this by -1 which is reverse full speed and like I said you can't go less otherwise counted as -1
     public Command negpowertwo = new SetPower(Parts.flywheel2, -1).requires(this);
-    public Command stopone = new SetPower(Parts.flywheel1, 0).requires(this);
+    public Command stopone = new SetPower(Parts.flywheel1, 0).requires(this); // stops giving power to motor
     public Command stoptwo = new SetPower(Parts.flywheel2, 0).requires(this);
+
 }
