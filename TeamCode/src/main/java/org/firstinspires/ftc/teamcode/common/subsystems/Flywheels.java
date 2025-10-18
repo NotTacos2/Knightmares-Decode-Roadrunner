9e4bd59c577f9abc811ae56abe238d13bf6bf584
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2025-10-17 21:26:23 -0700

    doesn't work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/Flywheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/Flywheels.java
@@ -13,22 +13,11 @@ public class Flywheels implements Subsystem {
 
     Parts part = new Parts();
 
-    ControlSystem flywheelsystem = ControlSystem.builder()
-            .posPid(0.02, 0, 0.02)
-            .build();
-
     public Command powerone = new SetPower(Parts.flywheel1, 1).requires(this);
-    public Command testpowerone = new SetPower(Parts.flywheel1,
-            flywheelsystem.calculate(
-                    new KineticState(Parts.flywheel1.getCurrentPosition(), Parts.flywheel1.getVelocity())
-            )).requires(this);
     public Command powertwo = new SetPower(Parts.flywheel2, 1).requires(this);
-    public Command testpowertwo = new SetPower(Parts.flywheel2,
-            flywheelsystem.calculate(
-                    new KineticState(Parts.flywheel2.getCurrentPosition(), Parts.flywheel2.getVelocity())
-            )).requires(this);
     public Command negpowerone = new SetPower(Parts.flywheel1, -1).requires(this);
     public Command negpowertwo = new SetPower(Parts.flywheel2, -1).requires(this);
     public Command stopone = new SetPower(Parts.flywheel1, 0).requires(this);
     public Command stoptwo = new SetPower(Parts.flywheel2, 0).requires(this);
+
 }
