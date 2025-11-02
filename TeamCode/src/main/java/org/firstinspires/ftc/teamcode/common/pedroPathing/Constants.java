994fc5d0e71d77026c5294d66fe7dc35dca35d7d
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2025-11-02 14:25:42 -0700

    code from scrim
    github takes like 67 years to upload code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/pedroPathing/Constants.java
@@ -3,19 +3,32 @@ package org.firstinspires.ftc.teamcode.common.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.Encoder;
 import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(7.257);
+            .mass(11.5);
             //.robotLength(8.5)
             //.robotWidth(13.5)
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rightFront")
+            .rightRearMotorName("rightRear")
+            .leftRearMotorName("leftRear")
+            .leftFrontMotorName("leftFront")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 
     public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
             // this is jacob's config so um yeah!!
@@ -23,12 +36,17 @@ public class Constants {
             .leftRearMotorName("leftRear")
             .rightFrontMotorName("rightFront")
             .rightRearMotorName("rightRear")
-            .leftFrontEncoderDirection(Encoder.REVERSE)
-            .leftRearEncoderDirection(Encoder.REVERSE);
+            .robotLength(8.5)
+            .robotWidth(13.5)
+            .leftFrontEncoderDirection(Encoder.FORWARD)
+            .leftRearEncoderDirection(Encoder.FORWARD)
+            .rightFrontEncoderDirection(Encoder.FORWARD)
+            .rightRearEncoderDirection(Encoder.FORWARD);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
                 .driveEncoderLocalizer(localizerConstants)
                 .build();
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/Flywheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/Flywheels.java
@@ -5,6 +5,8 @@ import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.hardware.controllable.RunToVelocity;
 import dev.nextftc.hardware.powerable.SetPower;
+import com.pedropathing.control.PIDFController;
+import com.pedropathing.control.PIDFCoefficients;
 import dev.nextftc.control.ControlSystem;
 import org.firstinspires.ftc.teamcode.common.Parts;
 
@@ -15,20 +17,22 @@ public class Flywheels implements Subsystem {
 
     Parts part = new Parts();
 
-    private final ControlSystem powercontrolled = ControlSystem.builder()
-            .velPid(0.011) // maybe??
+    // TODO: fix this not connecting to the dashboard
+    public final double proportionalgain = 0.015;
+    public final double velocity = 950.0;
+    public final ControlSystem powercontrolled = ControlSystem.builder()
+            .velPid(proportionalgain)
             .basicFF(0.003)
             .build();
 
     // idk if this is good or not
     public final Command off = new RunToVelocity(powercontrolled, 0.0).requires(this);
-    public final Command on = new RunToVelocity(powercontrolled, 850.0).requires(this);
-    public final Command reverse = new RunToVelocity(powercontrolled, -850.0).requires(this);
+    public final Command on = new RunToVelocity(powercontrolled, velocity).requires(this);
+    public final Command reverse = new RunToVelocity(powercontrolled, -velocity).requires(this);
 
     @Override
     public void periodic() {
         Parts.flywheel1.setPower(powercontrolled.calculate(Parts.flywheel1.getState()));
         Parts.flywheel2.setPower(powercontrolled.calculate(Parts.flywheel2.getState()));
     }
-
 }
