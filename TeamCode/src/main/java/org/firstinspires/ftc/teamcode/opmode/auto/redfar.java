2324371dcead5d8533f7a61761839dee0598d9cb
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2025-12-07 10:25:24 -0700

    might aswell

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/redfar.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.common.Parts;
+
+
+@Autonomous (name = "Red Far", group = "Decode")
+public class redfar extends LinearOpMode {
+    public void runOpMode() throws InterruptedException {
+        Parts config = new Parts(); // configure robot
+        waitForStart();
+
+
+        Parts.FL.setPower(0.5);
+        Parts.FR.setPower(0.5);
+        Parts.BL.setPower(0.5);
+        Parts.BR.setPower(0.5);
+        sleep(400);
+        Parts.FL.setPower(0);
+        Parts.FR.setPower(0);
+        Parts.BL.setPower(0);
+        Parts.BR.setPower(0);
+
+        Parts.flywheel1.setPower(0.82);
+        Parts.flywheel2.setPower(0.82);
+        sleep(2500);
+        Parts.intake.setPower(1);
+        Parts.intake2.setPower(-1);
+        sleep(500);
+        Parts.intake.setPower(0);
+        Parts.intake2.setPower(0);
+        sleep(2000);
+        Parts.intake.setPower(1);
+        Parts.intake2.setPower(-1);
+        sleep(500);
+        Parts.intake.setPower(0);
+        Parts.intake2.setPower(0);
+        sleep(2000);
+        Parts.intake.setPower(1);
+        Parts.intake2.setPower(-1);
+        sleep(2000);
+        Parts.flywheel1.setPower(0);
+        Parts.flywheel2.setPower(0);
+        Parts.intake.setPower(0);
+        Parts.intake2.setPower(0);
+        sleep(1000);
+
+        Parts.FL.setPower(0.5);
+        Parts.FR.setPower(0.5);
+        Parts.BL.setPower(-0.5);
+        Parts.BR.setPower(0.5);
+        sleep(500);
+        Parts.FL.setPower(0.5);
+        Parts.FR.setPower(0.5);
+        Parts.BL.setPower(0.5);
+        Parts.BR.setPower(0.5);
+        sleep(1000);
+        Parts.FL.setPower(0);
+        Parts.FR.setPower(0);
+        Parts.BL.setPower(0);
+        Parts.BR.setPower(0);
+    }
+
+
+}
