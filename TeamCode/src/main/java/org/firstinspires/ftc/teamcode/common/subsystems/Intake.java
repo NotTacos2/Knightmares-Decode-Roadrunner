cc60bcbb695a9374316a2158214f35aa743061b0
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2025-12-07 22:12:50 -0700

    why is this here

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/Intake.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.common.subsystems;
 
-import android.provider.Telephony;
-
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.hardware.powerable.SetPower;
@@ -19,6 +17,7 @@ public class Intake implements Subsystem {
     public Command rotationcon = new SetPower(Parts.intake2, 1).requires(this);
     public Command rotationconstop = new SetPower(Parts.intake2, 0).requires(this);
 }
+
 
 
 
